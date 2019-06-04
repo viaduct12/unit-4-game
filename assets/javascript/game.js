48ab61e038b9e26c6cc2c0e70677f2dff021e6e5
Lawrence Fiesta <lfiesta86@gmail.com>
2019-06-03 23:59:16 -0700

    created buttons for the chars

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var charChoices = {
+  
+  obi: {
+    base: 5,
+    newBase: 0,
+    hp: 100,
+    countAttk: 15
+  },
+  
+  luke: {
+    base: 5,
+    newBase: 0,
+    hp: 120,
+    countAttk: 25
+  },
+  
+  char3: {
+    base: 5,
+    newBase: 0,
+    hp: 140,
+    countAttk: 25
+  },
+  
+  char4: {
+    base: 5,
+    newBase: 0,
+    hp: 180,
+    countAttk: 25
+  }
+  
+};
+
+
